96bcb3eaf24112c28f4db230840291515494d705
djx-ctrl <cras.dtravis@gmail.com>
2023-01-03 05:24:59 -0800

    on/off

--- src/components/LightSwitch.js
+++ src/components/LightSwitch.js
@@ -1,9 +1,26 @@
-import React from "react"
+import React, {useState} from "react"
 
 const LightSwitch = () => {
+  
+  const [lightSwitchStatus, setLightSwitchStatus] = useState("off")
+  
+  const [colorOfBulb, setColorOfBulb] = useState("white")
+
+  const clicksLight = () => {
+    if(lightSwitchStatus === "off"){
+      setColorOfBulb("yellow")
+      setLightSwitchStatus("on")
+    } else {
+        setColorOfBulb("white")
+        setLightSwitchStatus("off")
+    }
+}
+  
   return (
     <>
       <div className="lightbulb"></div>
+    <div classname="lightbulb" onClick={clicksLight} style={{backgroundColor:colorOfBulb}}>{lightSwitchStatus}</div>
+    
     </>
   )
 }
